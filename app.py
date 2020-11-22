4e898932b98fd242eca15ddf50ee50de92733da5
CodingGirl <spunase@gmail.com>
2020-11-22 01:46:06 -0600

    user choice input

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from tensorflow.keras.models import load_model
 from keras.preprocessing import image
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -24,12 +25,17 @@ def predict():
     image_predict = image.load_img('templates/baklava.jpg', target_size=(64,64))
     image_predict = image.img_to_array(image_predict)
     image_predict = np.expand_dims(image_predict, axis=0)
-    # prediction = model.predict([[2, 9, 6]])
-      # output = round(prediction[0], 2)
-    prediction = model.predict(image_predict)
-  
-    output = prediction[0][0]
-    label = round(prediction[0][1])
+
+    y_prob = model.predict(image_predict) 
+    y_classes = int(y_prob.argmax(axis=-1))
+
+    PATH = os.getcwd()
+    img_folder_path = os.path.join(PATH, "images")
+
+    CATEGORY = os.listdir(img_folder_path)
+    print(CATEGORY, y_classes)
+
+    print("The class of the image is ", CATEGORY[y_classes])
 
     return render_template('index.html', prediction_text='The Image belongs to the classification of {}'.format(output), 
     prediction_label='The Image belongs to the classification of {}'.format(label))
