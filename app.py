f7d7d142fb6371ca1111767e8ebbd8eece64bc4c
CodingGirl <spunase@gmail.com>
2020-11-21 21:36:45 -0600

    added model.h5

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-import numpy as np
+
 from flask import Flask, request, jsonify, render_template
 from keras.models import load_model
 from keras.preprocessing import image
 import pickle
+import numpy as np
 
 app = Flask(__name__)
 
@@ -13,7 +14,7 @@ model = pickle.load(open('model.pkl','rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['POST'])  
 def predict():
     '''
     For rendering results on HTML GUI
@@ -23,13 +24,13 @@ def predict():
     image_predict = image.load_img('templates/baklava.jpg', target_size=(64,64))
     image_predict = image.img_to_array(image_predict)
     image_predict = np.expand_dims(image_predict, axis=0)
-    
     prediction = model.predict([[2, 9, 6]])
     output = round(prediction[0], 2)
-    # if prediction[0][0] == 1:
-    #     output = "Found it!"
-    # else:
-    #     output = "Image classification is tough. Train me more!!"
+    if prediction[0] == 1:
+        output = "Found it!"
+    else:
+        output = "Image classification is tough. Train me more!!"
+    print(output)    
 
     return render_template('index.html', prediction_text='The Image belongs to the classification of {}'.format(output))
 
